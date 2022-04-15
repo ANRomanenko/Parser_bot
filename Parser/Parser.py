ae5b30118fc7b2bbb6bb3cb4440933291dd31bc7
andredance <lifeapricot@gmail.com>
2022-04-15 22:20:29 +0300

    Parser

--- Parser/Parser.py
+++ Parser/Parser.py
@@ -1,11 +1,31 @@
+import datetime
 import requests
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 
 
-def first_version(x, y, z):
-    a = x + y
-    b = x + z
-    c = y + z
-    print(a, b, c)
 
-first_version(1, 2, 3)
+def collect_data(city_code='506'):
+    cur_time = datetime.datetime.now().strftime('%d_%m_%Y_%H_%M')
+    ua = UserAgent()
+
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'User-Agent': ua.random
+    }
+
+    cookies = {
+        'active_city_id': f'{city_code}'
+    }
+
+    response = requests.get(url='https://comfy.ua/smartfon/brand__apple/', headers=headers, cookies=cookies)
+
+    with open(f'index.html', 'w', encoding='utf-8') as file:
+        file.write(response.text)
+
+def main():
+    collect_data(city_code='506')
+
+
+if __name__ == '__main__':
+    main()
