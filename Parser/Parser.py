c00801d7baeb2fa7ec1669bf314223c79d602662
andredance <lifeapricot@gmail.com>
2022-05-20 23:33:18 +0300

    bot

--- Parser/Parser.py
+++ Parser/Parser.py
@@ -1,13 +1,14 @@
 import datetime
 import requests
+import csv
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
 
 
-def collect_data(city_code='506'):
-    cur_time = datetime.datetime.now().strftime('%d_%m_%Y_%H_%M')
-    ua = UserAgent()
+def collect_data(city_code='506'): # Принимает параметер код города!
+    cur_time = datetime.datetime.now().strftime('%d_%m_%Y_%H_%M') # День, месяц, год, и часы с минутами
+    ua = UserAgent() # Объект класса
 
     headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
@@ -18,19 +19,31 @@ def collect_data(city_code='506'):
         'active_city_id': f'{city_code}'
     }
 
-    # response = requests.get(url='https://comfy.ua/smartfon/brand__apple/', headers=headers, cookies=cookies)
+    response = requests.get(url='https://comfy.ua/smartfon/brand__apple/', headers=headers, cookies=cookies)
     #
     # with open(f'index.html', 'w', encoding='utf-8') as file:
     #     file.write(response.text)
 
-    with open('index.html', encoding='utf-8') as file:
-        src = file.read()
+    # with open('index.html', encoding='utf-8') as file:
+    #     src = file.read()
 
-    soup = BeautifulSoup(src, 'lxml')
+    soup = BeautifulSoup(response.text, 'lxml')
 
     city = soup.find('span', class_='header-top__city-name icon-comfy header-top__more-icon').get_text(strip=True)
     cards = soup.find_all('div', class_='products-list-item products-catalog-grid__item products-list-item--grid')
     # print(city, len(cards))
+
+    with open(f'{city}_{cur_time}.csv', 'w', encoding='utf-32') as file:
+        writer = csv.writer(file, delimiter=';')
+
+        writer.writerow(
+            (
+                'Продукт',
+                'Старая цена',
+                'Новая цена',
+                'Процент скидки'
+            )
+        )
 
     for card in cards:
         card_title = card.find('a', class_='products-list-item__name').get_text(strip=True)
@@ -40,12 +53,26 @@ def collect_data(city_code='506'):
         except AttributeError:
             continue
 
-        price_old = card.find('div', class_='products-list-item__actions-price-old').get_text(strip=True).strip('span')
+        price_old = card.find('div', class_='products-list-item__actions-price-old').get_text(strip=True) # Старая цена
+        price_current = card.find('div', class_='products-list-item__actions-price-current').get_text(strip=True).replace('₴', '') # Новая цена
 
         # description = card.find('div', class_='list-item__specifications-text').get_text(strip=True).replace('iPhone 13', ' ').replace('• 09.2021', ' ').replace('• Модель з лінійки ', ' ').strip()
 
+        # print(price_current)
 
-        print(price_old)
+        with open(f'{city}_{cur_time}.csv', 'a', encoding='utf-32') as file:
+            writer = csv.writer(file, delimiter=';')
+
+            writer.writerow(
+                (
+                    card_title,
+                    price_old,
+                    price_current,
+                    card_discount
+                )
+            )
+
+    print(f'Файл {city}_{cur_time}.csv успешно записан!')
 
 def main():
     collect_data(city_code='506')
